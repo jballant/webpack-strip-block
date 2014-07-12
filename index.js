0b5c7b5b81593c86736ae23811939110a71676cf
James Ballantine <james@1stdibs.com>
2014-07-12 12:29:25 -0400

    Remove unused options

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 "use strict";
 
 function StripBlockLoader(content) {
-    var options = {};
-    this.startComment = options.startComment || 'develblock:start';
-    this.endComment = options.endComment || 'develblock:end';
+
+    this.startComment = 'develblock:start';
+    this.endComment = 'develblock:end';
 
     this.regexPattern = new RegExp("[\\t ]*\\/\\* ?" + this.startComment + " ?\\*\\/[\\s\\S]*?\\/\\* ?" + this.endComment + " ?\\*\\/[\\t ]*\\n?", "g");
 
