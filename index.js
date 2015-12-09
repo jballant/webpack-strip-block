e1ee94f6b3a3d7a27bc146b9c263f4ae40d23f1e
Jamie Ballantine <jballant@gmail.com>
2015-12-08 23:49:33 -0500

    Make sure webpack knows result is cacheable

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function StripBlockLoader(content) {
     this.regexPattern = new RegExp("[\\t ]*\\/\\* ?" + this.startComment + " ?\\*\\/[\\s\\S]*?\\/\\* ?" + this.endComment + " ?\\*\\/[\\t ]*\\n?", "g");
 
     content = content.replace(this.regexPattern, '');
+    
+    if (this.cacheable) {
+        this.cacheable(true);
+    }
 
     this.callback(null, content);
 }
