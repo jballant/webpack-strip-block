1a680b0aedf082c8058e5b1e23a7498e756fdbdf
nas <nas@nas>
2020-04-18 14:27:35 +0200

    Support multiple comment delimiters

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 /*jslint node:true */
-"use strict";
+'use strict';
 
-var loaderUtils = require("loader-utils");
+var loaderUtils = require('loader-utils');
+
+function regexEscape(str) {
+    return str.replace(/([\^|\$|\.|\*|\+|\?|\=|\!|\:|\\|\/|\(|\)|\[|\]|\{|\}])/gi, '\\$1');
+}
 
 function StripBlockLoader(content) {
-    var options = loaderUtils.getOptions(this) || {};
-    var startComment = options.start || 'develblock:start';
-    var endComment = options.end || 'develblock:end';
-
-    var regexPattern = new RegExp("[\\t ]*\\/\\* ?" + startComment + " ?\\*\\/[\\s\\S]*?\\/\\* ?" + endComment + " ?\\*\\/[\\t ]*\\n?", "g");
-
-    content = content.replace(regexPattern, '');
+    var options = loaderUtils.getOptions(this);
+    if (options && options.blocks) {
+        var start = Array.isArray(options.start) ? options.start : [options.start];
+        var end = Array.isArray(options.end) ? options.end : [options.end];
+        for(var i = 0; i < start.length; i++)
+        {
+            var starti = regexEscape(start[i] || '/*');
+            var endi = regexEscape(end[i] || '/*');
+            options.blocks.forEach(function (block) {
+                var regex = new RegExp('[\\t ]*' + starti + ' ?(' + block + '):start ?' + endi + '[\\s\\S]*?' + starti + ' ?\\1:end ?' + endi + '[\\t ]*\\n?', 'g');
+                content = content.replace(regex, '');
+            });
+        }
+    }
 
     if (this.cacheable) {
         this.cacheable(true);
